30d6ab7fbb2cd9996c5eeee1471cbddd8069d914
N3onTechF0X <128133812+N3onTechF0X@users.noreply.github.com>
2023-11-09 01:11:51 +0200

    Update utilities.user.js

--- utilities.user.js
+++ utilities.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Utilities
 // @description  Some of functions
-// @version      1.2
+// @version      1.3
 // @author       N3onTechF0X
 // @match        https://*.tankionline.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
