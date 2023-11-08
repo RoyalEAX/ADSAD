f7b15d68ef9f8a8314a5be751fb910320b3d8109
N3onTechF0X <128133812+N3onTechF0X@users.noreply.github.com>
2023-11-09 01:12:56 +0200

    Update utilities.src.js

--- utilities.src.js
+++ utilities.src.js
@@ -1,7 +1,8 @@
 const floatingWindow = document.createElement('div');
-floatingWindow.style.cssText = 'position: fixed; top: 20px; left: 20px; background-color: black; padding: 10px; border: 4px solid #8B00FF; display: none; border-radius: 15px; z-index: 9999; color: #8B00FF; font-size: 20px;';
+floatingWindow.style.cssText = 'position: fixed; top: 20px; left: 20px; background-color: black; padding: 10px; border: 4px solid #8B00FF; display: none; border-radius: 15px; z-index: 9999; font-size: 20px; color: black;';
 const toggle1 = document.createElement('div');
 toggle1.textContent = 'Открыть контейнеры';
+toggle1.style.cssText = "background-color: #8B00FF; border-radius: 5px; text-align: center;";
 toggle1.addEventListener('click', () => {
     floatingWindow.style.display = 'none';
     for (var i = 0; i < 1000; i++) {
@@ -20,20 +21,23 @@ function buy_conts() {
 }
 const toggle2 = document.createElement('div');
 toggle2.textContent = 'Купить контейнеры';
+toggle2.style.cssText = "background-color: red; border-radius: 5px; text-align: center;";
 toggle2.addEventListener('click', () => {
     is_conts_buy = !is_conts_buy;
     if (is_conts_buy) {
         conts_interval = setInterval(buy_conts, 200);
+        toggle2.style.backgroundColor = "green";
     } else {
         clearInterval(conts_interval);
+        toggle2.style.backgroundColor = "red";
     }
 });
 let is_conts_buy = false;
 let golds_interval;
 function hide_golds() {
-    const targetElements = document.querySelectorAll('.ksc-0.BattleMessagesComponentStyle-messageRow');
+    const targetElements = document.querySelectorAll('.BattleMessagesComponentStyle-messageRow');
     targetElements.forEach((element) => {
-        const message = element.querySelector('.ksc-0.BattleMessagesComponentStyle-message');
+        const message = element.querySelector('.BattleMessagesComponentStyle-message');
         if (message) {
             const innerHTML = message.innerHTML.toLowerCase();
             if (
@@ -51,16 +55,19 @@ function hide_golds() {
 }
 const toggle3 = document.createElement('div');
 toggle3.textContent = 'Спрятать голды';
+toggle3.style.cssText = "background-color: red; border-radius: 5px; text-align: center;";
 toggle3.addEventListener('click', () => {
     is_golds_hide = !is_golds_hide;
     if (is_golds_hide) {
         golds_interval = setInterval(hide_golds, 10);
+        toggle3.style.backgroundColor = "green";
     } else {
         clearInterval(golds_interval);
-        const targetElements = document.querySelectorAll('.ksc-0.BattleMessagesComponentStyle-messageRow');
+        const targetElements = document.querySelectorAll('.BattleMessagesComponentStyle-messageRow');
         targetElements.forEach((element) => {
             element.style.display = 'flex';
         });
+        toggle3.style.backgroundColor = "red";
     }
 });
 let is_golds_hide = false;
