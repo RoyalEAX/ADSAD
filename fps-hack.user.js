473ac2400181ee2740442cc61c35c8b330840708
N3onTechF0X <128133812+N3onTechF0X@users.noreply.github.com>
2023-11-12 14:54:01 +0200

    Update fps-hack.user.js

--- fps-hack.user.js
+++ fps-hack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FPS hack
 // @description  Upgrade your fps in battle
-// @version      1.0
+// @version      1.2
 // @author       N3onTechF0X
 // @match        https://*.tankionline.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
@@ -9,4 +9,4 @@
 // @grant        none
 // ==/UserScript==
 
-var i=!1;setInterval(()=>{var e=document.querySelector('.BattleHudFpsComponentStyle-value');if(!e){var i=!1;return 0;}if(!i){var n=document.createElement('span');n.textContent='144';n.style.color='rgb(116, 186, 61)';e.style.display='none';e.parentNode.insertBefore(n,e.nextSibling);i=!0;}},100);
+var i=!1;setInterval(()=>{var e=document.querySelector(".BattleHudFpsComponentStyle-value");if(!e)return i=!1;if(!i){var t=document.createElement("span");t.textContent="144",t.style.color="rgb(116, 186, 61)",e.style.display="none",e.parentNode.insertBefore(t,e.nextSibling),i=!0}},100);
