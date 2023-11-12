07c7b0af4e9aec89e1084c96d387b274111b697e
N3onTechF0X <128133812+N3onTechF0X@users.noreply.github.com>
2023-11-12 14:44:23 +0200

    Update fps-hack.user.js

--- fps-hack.user.js
+++ fps-hack.user.js
@@ -9,4 +9,4 @@
 // @grant        none
 // ==/UserScript==
 
-var i=!1;setInterval(()=>{var e=document.querySelector('.BattleHudFpsComponentStyle-value');if(!e){var i=!1;return 0;}if(!i){var n=document.createElement('span');n.textContent='144';n.style.color='rgb(116, 186, 61)';e.style.display='none';e.parentNode.insertBefore(newElement,element.nextSibling);i=!0;}},100);
+var i=!1;setInterval(()=>{var e=document.querySelector('.BattleHudFpsComponentStyle-value');if(!e){var i=!1;return 0;}if(!i){var n=document.createElement('span');n.textContent='144';n.style.color='rgb(116, 186, 61)';e.style.display='none';e.parentNode.insertBefore(n,e.nextSibling);i=!0;}},100);
