d8263d0182272e827b92dc8aab8b7ac08b7b7ef4
N3onTechF0X <128133812+N3onTechF0X@users.noreply.github.com>
2024-01-27 03:11:57 +0200

    Update Renda.user.js

--- Renda.user.js
+++ Renda.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Renda
 // @description  Powerfull clicker for Tanki Online
-// @version      0.7
+// @version      0.8
 // @author       N3onTechF0X
 // @match        https://*.tankionline.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
+// @icon         https://cdn.discordapp.com/attachments/1154469342671548456/1200608934331961464/N3onTechF0X.png
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
